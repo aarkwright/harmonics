36bca5154783712806fb0b571763c58c2ac46c80
aarkwright <apollo.arkwright@gmail.com>
2019-11-06 16:53:19 +0200

    Included some per-note frequency gathering. Looking at chords right now. Need to check if I can calculate inter-harmonic frequency modulation somehow.

--- main.py
+++ main.py
@@ -5,19 +5,49 @@ from math import log, log2, pow
 
 # Get standard tuning A4 and bottom C
 A4 = 440
-C0 = A4*pow(2, -4.75)
+C0 = A4 * pow(2, -4.75)
 
 note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 note_variants = [None, "Db", None, "Eb", None, None, "Gb", None, "Ab", None, "Bb", None]
 
 
-def get_note(freq):
-    # half_steps = round(12*log2(freq/C0))
-    half_steps = round(12 * log(freq / C0, 2))
-    octave = half_steps // 12
-    n = half_steps % 12
-    return "{}{}".format(note_names[n], octave)
+def get_freq(note: str = 'A4'):
+    assert isinstance(note, str)
+    return m21.note.Note(note).pitch.frequency
 
-def get_freq(note, octave):
-    index = note_names.index(note)
-    half_steps = octave * 12
+
+def get_note_harmonics(note: str, count: int) -> tuple:
+    """
+    :param note: (string) The note you want the harmonics for
+    :param count: (int) The number of harmonics you want to be returned
+    :return: (list) Tuples with harmonic note name and respective frequency
+    """
+    assert isinstance(note, str)
+    assert isinstance(count, int)
+
+    harmonics = []
+    harmonic_indexes = range(2, count + 2)
+
+    for i in harmonic_indexes:
+        harmonic = m21.note.Note(note).pitch.getHarmonic(i)
+        harmonics.append(
+            (
+                "{}{}".format(harmonic.name, harmonic.octave),
+                harmonic.frequency,
+                harmonic.frequency % 440,
+                harmonic.frequency / 440
+            )
+        )
+
+    return tuple(harmonics)
+
+# Example
+harmonics = []
+for note in ('C4', 'E4', 'G4'):
+    harmonics.append(
+        [note,
+        get_note_harmonics(note, 3)]
+    )
+
+for i in harmonics:
+    print("Note:\t{}\nHarmonics:\t{}\n".format(i[0], [(x[0], x[1]) for x in i[1]]))
